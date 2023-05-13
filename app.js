bf4aa3d2633897e3f5a8cd7623ad880f83e7ec45
Akshit Singh Chauhan <aschauhan2325@gmail.com>
2023-05-13 17:47:09 +0530

    adding app.js

---
+++ app.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const express = require("express");
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+
+// loading env variables
+dotenv.config();
+// to do - mongodb connection file
+// const db = require("./db");
+
+const app = express();
+
+app.use(require("cors")());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// to do add routes
+//app.use("/v1", require("./routes"));
+
+// 404 route not found
+app.use((req, res, next) => {
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  if (err) {
+    res.status(err.status || 500);
+    res.json({
+      errors: {
+        message: err.message,
+        error: err,
+      },
+    });
+  }
+});
+
+var server = app.listen(process.env.PORT || 3000, () => {
+  console.log(`Listening on port ${server.address().port}`);
+});
